f2b3e3183658eafd0fc8ded8dab6b2426ba19b87
Itziar Sesma <itziar.sesma.u@gmail.com>
2017-03-18 22:59:29 +0100

    Se añade al api de blogs la ordenación y el filtrado

--- src/blogs/api_rest/api.py
+++ src/blogs/api_rest/api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet, GenericViewSet
@@ -9,3 +10,6 @@ from blogs.api_rest.serializers import BlogSerializer
 class BlogsAPI(ListAPIView):
     queryset = User.objects.all().values('username')
     serializer_class = BlogSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ("username", )
+    ordering_fields = ("username", )
