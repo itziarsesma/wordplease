cff795bcea46d4038559b3a04c61d46bbdf454b4
Itziar Sesma <itziar.sesma.u@gmail.com>
2017-03-18 19:43:07 +0100

    Se crea el proyecto con la parte web y el api rest de usuarios

---
+++ src/login/api_rest/api.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from rest_framework.viewsets import ModelViewSet
+
+from login.api_rest.permissions import UsersPermission
+from login.api_rest.serializers import UsersListSerializer, UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = (UsersPermission,)
+
+    def get_serializer_class(self):
+        return UsersListSerializer if self.action == "list" else UserSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
